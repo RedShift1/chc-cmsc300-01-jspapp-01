a9a21807ed979e4fe5907b02d86d0d18960038c5
Alexander <a.m.brandstrup@gmail.com>
2015-09-16 20:40:07 +0200

    Added the get/Json to UserController

---
+++ Admonitus/src/Controllers/User.java
@@ -0,0 +1,45 @@
+package Controllers;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import flexjson.JSONSerializer;
+import MVC.Controller;
+
+/**
+ * @author Alexander
+ *
+ */
+public class User extends Controller {
+
+	@Override
+	public void doRequest(String actionName, Integer id) throws Exception {
+
+		EntityManager em = this.getServletContext().getEM();
+
+		// if(actionName.equals("create")) {
+		//
+		// User u = new User();
+		//
+		// // u.setEmail(getRequest().getParameter("email"));
+		//
+		// em.getTransaction().begin();
+		// em.persist(u);
+		// em.getTransaction().commit();
+
+		//
+		if (actionName.equals("get")) {
+			List<?> list = em.createNamedQuery("User.findAll").getResultList();
+
+			JSONSerializer serializer = new JSONSerializer();
+			this.getRequest().setAttribute("userList",
+					serializer.serialize(list));
+
+			forward("/json.jsp");
+
+		}
+
+	}
+
+}
