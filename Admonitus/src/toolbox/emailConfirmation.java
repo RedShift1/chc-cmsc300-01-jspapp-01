fd4aa2066848964be162583d5a93dd1a503eba83
AlexanderB92 <a.m.brandstrup@gmail.com>
2015-10-01 21:30:44 +0200

    Added email class
    Added fields

---
+++ Admonitus/src/toolbox/emailConfirmation.java
@@ -0,0 +1,50 @@
+package toolbox;
+import java.util.*;
+import javax.activation.*;
+
+public class emailConfirmation {
+	
+	private String to;
+	private String from;
+	private String subject;
+	private String text;
+	private String senderMail;
+	private String senderPassword;
+	private String recipientMail;
+	
+	public emailConfirmation() {
+		
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
