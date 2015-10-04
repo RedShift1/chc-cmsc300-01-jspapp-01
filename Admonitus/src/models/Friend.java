d79a0980d527e1ef4c62c6550e581f523eac4ac2
Glenn Matthys <glenn@webmind.be>
2015-10-04 16:34:27 -0400

    .settings/org.eclipse.jpt.entitygen.mysql-localhost-admonitus2.admonitus Add friend model

---
+++ Admonitus/src/models/Friend.java
@@ -0,0 +1,64 @@
+package models;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import models.Reminder;
+
+/**
+ * The persistent class for the friends database table.
+ * 
+ */
+@Entity
+@Table(name = "friends")
+@NamedQuery(name = "Friend.findAll", query = "SELECT f FROM Friend f")
+public class Friend implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String email;
+
+    // bi-directional many-to-one association to Reminder
+    @ManyToOne
+    private Reminder reminder;
+
+    public Friend()
+    {
+    }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public Reminder getReminder()
+    {
+        return this.reminder;
+    }
+
+    public void setReminder(Reminder reminder)
+    {
+        this.reminder = reminder;
+    }
+
+}
