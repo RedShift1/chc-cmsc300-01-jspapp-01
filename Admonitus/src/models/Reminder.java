21ddf78b915da3ac65238e31ca0c946100e0720d
Glenn Matthys <glenn@webmind.be>
2015-10-04 16:33:49 -0400

    .settings/org.eclipse.jpt.entitygen.mysql-localhost-admonitus2.admonitus Add friend datastructures to Reminder

--- Admonitus/src/models/Reminder.java
+++ Admonitus/src/models/Reminder.java
@@ -4,107 +4,155 @@ import java.io.Serializable;
 
 import javax.persistence.*;
 
+import models.Friend;
 
 import java.util.Date;
+import java.util.List;
 import java.sql.Timestamp;
-
 
 /**
  * The persistent class for the reminders database table.
  * 
  */
 @Entity
-@Table(name="reminders")
-@NamedQuery(name="Reminder.findAll", query="SELECT r FROM Reminder r")
-public class Reminder implements Serializable {
-	private static final long serialVersionUID = 1L;
+@Table(name = "reminders")
+@NamedQuery(name = "Reminder.findAll", query = "SELECT r FROM Reminder r")
+public class Reminder implements Serializable
+{
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-	@Temporal(TemporalType.DATE)
-	@Column(name="creation_date")
-	private Date creationDate;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "creation_date")
+    private Date creationDate;
 
-	@Temporal(TemporalType.DATE)
-	private Date datestart;
+    @Temporal(TemporalType.DATE)
+    private Date datestart;
 
-	private byte frequency;
+    private byte frequency;
 
-	@Column(name="last_update")
-	private Timestamp lastUpdate;
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
 
-	@Lob
-	private String text;
+    @Lob
+    private String text;
 
-	//bi-directional many-to-one association to User
-	@ManyToOne
-	private User user;
+    // bi-directional many-to-one association to User
+    @ManyToOne
+    private User user;
 
-	public Reminder() {
-	}
+    // bi-directional many-to-one association to Friend
+    @OneToMany(mappedBy = "reminder")
+    private List<Friend> friends;
 
-	public int getId() {
-		return this.id;
-	}
+    public Reminder()
+    {
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId()
+    {
+        return this.id;
+    }
 
-	public Date getCreationDate() {
-		return this.creationDate;
-	}
+    public void setId(int id)
+    {
+        this.id = id;
+    }
 
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
+    public Date getCreationDate()
+    {
+        return this.creationDate;
+    }
 
-	public Date getDatestart() {
-		return this.datestart;
-	}
+    public void setCreationDate(Date creationDate)
+    {
+        this.creationDate = creationDate;
+    }
 
-	public void setDatestart(Date datestart) {
-		this.datestart = datestart;
-	}
+    public Date getDatestart()
+    {
+        return this.datestart;
+    }
 
-	public byte getFrequency() {
-		return this.frequency;
-	}
+    public void setDatestart(Date datestart)
+    {
+        this.datestart = datestart;
+    }
 
-	public void setFrequency(byte frequency) {
-		this.frequency = frequency;
-	}
+    public byte getFrequency()
+    {
+        return this.frequency;
+    }
 
-	public Timestamp getLastUpdate() {
-		return this.lastUpdate;
-	}
+    public void setFrequency(byte frequency)
+    {
+        this.frequency = frequency;
+    }
 
-	public void setLastUpdate(Timestamp lastUpdate) {
-		this.lastUpdate = lastUpdate;
-	}
+    public Timestamp getLastUpdate()
+    {
+        return this.lastUpdate;
+    }
 
-	public String getText() {
-		return this.text;
-	}
+    public void setLastUpdate(Timestamp lastUpdate)
+    {
+        this.lastUpdate = lastUpdate;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public String getText()
+    {
+        return this.text;
+    }
 
-	public User getUser() {
-		return this.user;
-	}
+    public void setText(String text)
+    {
+        this.text = text;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
-	
-	@Override
-	public String toString() {
-		return text;
-		
-	}
+    public User getUser()
+    {
+        return this.user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    public List<Friend> getFriends()
+    {
+        return this.friends;
+    }
+
+    public void setFriends(List<Friend> friends)
+    {
+        this.friends = friends;
+    }
+
+    public Friend addFriend(Friend friend)
+    {
+        getFriends().add(friend);
+        friend.setReminder(this);
+
+        return friend;
+    }
+
+    public Friend removeFriend(Friend friend)
+    {
+        getFriends().remove(friend);
+        friend.setReminder(null);
+
+        return friend;
+    }
+
+    @Override
+    public String toString()
+    {
+        return text;
+
+    }
 
 }
