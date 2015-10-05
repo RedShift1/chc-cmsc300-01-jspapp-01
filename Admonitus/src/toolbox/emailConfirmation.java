b4a8774479b03526b72a379231ee9e39edac7e8b
AlexanderB92 <a.m.brandstrup@gmail.com>
2015-10-05 21:02:20 +0200

    Restructured the emailConfirmationclass
    Created constructor, get/set methods and sendMail method.

--- Admonitus/src/toolbox/emailConfirmation.java
+++ Admonitus/src/toolbox/emailConfirmation.java
@@ -8,12 +8,20 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class emailConfirmation {
-	public static void main(String[] args) {
 
-		String to = "a.m.brandstrup@gmail.com";
-		String from = "a.m.brandstrup@gmail.com";
-		String subject = "Test Mail";
-		String text = "Yo, tester";
+	String to;
+	String from;
+	String subject;
+	String text;
+
+	public emailConfirmation(String to, String from, String subject, String text) {
+		this.to = to;
+		this.from = from;
+		this.subject = subject;
+		this.text = text;
+	}
+
+	public void sendEmail() {
 		Properties properties = System.getProperties();
 		properties.put("mail.smtp.starttls.enable", "true");
 		properties.put("mail.smtp.host", "smtp.gmail.com");
@@ -21,32 +29,64 @@ public class emailConfirmation {
 		properties.put("mail.smtp.password", "anotherchancetonight");
 		properties.put("mail.smtp.port", "587");
 		properties.put("mail.smtp.auth", "true");
-		
-		Session session = Session.getDefaultInstance(properties, 
-			    new javax.mail.Authenticator(){
-			        protected PasswordAuthentication getPasswordAuthentication() {
-			            return new PasswordAuthentication(
-			                "admonitusconfirmation@gmail.com", "anotherchancetonight");
-			        }
-			});
-		
-		try{
+
+		Session session = Session.getDefaultInstance(properties,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(
+								"admonitusconfirmation@gmail.com",
+								"anotherchancetonight");
+					}
+				});
+
+		try {
 			MimeMessage message = new MimeMessage(session);
-			
+
 			message.setFrom(new InternetAddress(from));
-			
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			
+
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					to));
+
 			message.setSubject(subject);
-			
+
 			message.setText(text);
-			
+
 			Transport.send(message);
 			System.out.println("Email sent");
-		}catch (MessagingException e) {
+		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
-
 	}
 
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
 }
