57b7b7e9a3d3107decac4156a98a54d98493fee9
AlexanderB92 <a.m.brandstrup@gmail.com>
2015-10-06 21:21:34 +0200

    Send confirmation functionality added to adding, deleting, editting reminders.

--- Admonitus/src/Controllers/UserCtl.java
+++ Admonitus/src/Controllers/UserCtl.java
@@ -136,7 +136,9 @@ public class UserCtl extends Controller {
                 em.getTransaction().commit();
                 
                 response = new JSONResponse(true, null, null);
-                emailConfirmation conf = new emailConfirmation(this.getRequest().getParameter("email"), "Admonitus", "Confirmation Message", "Your account has been succesfully created, thank you");
+                
+                User u = (User) this.getRequest().getSession().getAttribute("user");
+                emailConfirmation conf = new emailConfirmation(u.getEmail(), "Admonitus", "Confirmation Message", "Your account has been succesfully created, thank you");
        		 	conf.sendEmail();
             }
             catch (Exception ex)
