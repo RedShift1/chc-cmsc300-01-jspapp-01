a723200801431b5591d6a91bd7a834f535327fc3
Glenn Matthys <glenn@webmind.be>
2015-10-07 19:20:57 -0400

    Implement get/set picture for Users

--- Admonitus/src/Controllers/UserCtl.java
+++ Admonitus/src/Controllers/UserCtl.java
@@ -1,17 +1,33 @@
 package Controllers;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 
+import models.User;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import toolbox.JSONResponse;
 import toolbox.Sha;
-import models.User;
-import flexjson.JSONSerializer;
 import MVC.Controller;
+import flexjson.JSONSerializer;
 
 /**
  * @author Alexander
@@ -170,6 +186,158 @@ public class UserCtl extends Controller {
             forward("/json.jsp");
         }
 
+        if(actionName.equals("getPicture"))
+        {
+            
+            User u = (User) this.getRequest().getSession().getAttribute("user");
+            byte[] picture;
+            Controllers.Image image;
+            if((picture = u.getPicture()) == null)
+            {
+                image = new Controllers.Image("");
+                image.setContent(new FileInputStream(Paths.get(this.getRequest().getSession().getServletContext().getRealPath("/commons/defaultpicture.png")).toString()));
+            }
+            else
+            {
+                image = new Controllers.Image("foobar");
+                image.setContent(new ByteArrayInputStream(picture));
+            }
+                
+                
+            // http://stackoverflow.com/questions/2979758/writing-image-to-servlet-response-with-best-performance
+            // http://stackoverflow.com/questions/2340406/how-to-retrieve-and-display-images-from-a-database-in-a-jsp-page/2341322#2341322
+             // Get Image from DB.
+
+
+         this.getResponse().setHeader("Content-Type", this.getServletContext().getServletContext().getMimeType(image.getFilename()));
+         this.getResponse().setHeader("Content-Length", String.valueOf(image.getLength()));
+         this.getResponse().setHeader("Content-Disposition", "inline; filename=\"" + image.getFilename() + "\"");
+
+         BufferedInputStream input = null;
+         BufferedOutputStream output = null;
+
+         try {
+             input = new BufferedInputStream(image.getContent());
+             output = new BufferedOutputStream(this.getResponse().getOutputStream());
+             byte[] buffer = new byte[8192];
+             for (int length = 0; (length = input.read(buffer)) > 0;) {
+                 output.write(buffer, 0, length);
+             }
+         } finally {
+             if (output != null) try { output.close(); } catch (IOException logOrIgnore) {}
+             if (input != null) try { input.close(); } catch (IOException logOrIgnore) {}
+         }
+        }
+        
+        if(actionName.equals("setPicture"))
+        {
+            /*if(!loggedIn())
+            {
+                return;
+            }*/
+            /*
+            User u = (User) this.getRequest().getSession().getAttribute("user");
+            
+            
+            boolean isMultipart = ServletFileUpload.isMultipartContent(this.getRequest());
+
+            if (isMultipart) {
+
+                try {
+                    
+                    
+                    Collection<Part> parts = this.getRequest().getParts();
+                    
+                    for(Part p : parts)
+                    {
+                        System.out.println("Part name: " + p.getName());
+                    }
+                    
+                    Part filePart = this.getRequest().getPart("undefined");
+                                        
+                    Enumeration names = this.getRequest().getParameterNames();
+                    while(names.hasMoreElements())
+                    {
+                        String aName = (String) names.nextElement();
+                        System.out.println(aName + ": " + this.getRequest().getParameter(aName));
+                    }
+
+                    
+                    InputStream filecontent = filePart.getInputStream();
+                    
+                    byte[] buffer = new byte[8192];
+                    int bytesRead;
+                    ByteArrayOutputStream output = new ByteArrayOutputStream();
+                    while ((bytesRead = filecontent.read(buffer)) != -1)
+                    {
+                        output.write(buffer, 0, bytesRead);
+                    }
+                    u.setPicture(output.toByteArray());
+                    
+
+                    em.getTransaction().begin();
+                    em.merge(u);
+                    em.getTransaction().commit();
+
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }*/            
+            
+            User u = (User) this.getRequest().getSession().getAttribute("user");
+            
+            boolean isMultipart = ServletFileUpload.isMultipartContent(this.getRequest());
+            
+            if (isMultipart) {
+                System.out.println("Is multipart");
+                    // Create a factory for disk-based file items
+                    FileItemFactory factory = new DiskFileItemFactory();
+
+                    // Create a new file upload handler
+                    ServletFileUpload upload = new ServletFileUpload(factory);
+     
+                try {
+                    // Parse the request
+                    List /* FileItem */ items = upload.parseRequest(this.getRequest());
+                    Iterator iterator = items.iterator();
+                    while (iterator.hasNext()) {
+                        FileItem item = (FileItem) iterator.next();
+                        if (!item.isFormField()) {
+                            // Assume the first file is the picture
+                            InputStream filecontent = item.getInputStream();
+                            
+                            byte[] buffer = new byte[8192];
+                            int bytesRead;
+                            ByteArrayOutputStream output = new ByteArrayOutputStream();
+                            while ((bytesRead = filecontent.read(buffer)) != -1)
+                            {
+                                output.write(buffer, 0, bytesRead);
+                            }
+                            u.setPicture(output.toByteArray());
+                            
+
+                            em.getTransaction().begin();
+                            em.merge(u);
+                            em.getTransaction().commit();
+
+                            return;
+                        }
+                    }
+                } catch (FileUploadException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+            {
+                System.out.println("No multipart");
+            }
+            
+        }
+        
     }
 
 }
