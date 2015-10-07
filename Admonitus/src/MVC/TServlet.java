005bb4f03b8c781e679ba5f540c2b45ac600f95d
Glenn Matthys <glenn@webmind.be>
2015-10-07 14:17:28 -0400

    Annotate TServlet as WebServlet and MultipartConfig

--- Admonitus/src/MVC/TServlet.java
+++ Admonitus/src/MVC/TServlet.java
@@ -8,6 +8,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +18,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Glenn
  */
+@WebServlet
+@MultipartConfig
 public class TServlet extends HttpServlet
 {
     private static final long serialVersionUID = -2377400074458457448L;
