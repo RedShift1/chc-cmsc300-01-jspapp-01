f530148cd9d6edfc8c2e4d967e108211e96b7d6a
Glenn Matthys <glenn@webmind.be>
2015-10-07 21:09:09 -0400

    Fix indentation in emailConfirmation

--- Admonitus/src/toolbox/emailConfirmation.java
+++ Admonitus/src/toolbox/emailConfirmation.java
@@ -9,76 +9,76 @@ import javax.mail.internet.MimeMessage;
 
 public class emailConfirmation {
 
-	String to;
-	String from;
-	String subject;
-	String action;
+    String to;
+    String from;
+    String subject;
+    String action;
 
-	public emailConfirmation(String to, String from, String subject, String action) {
-		this.to = to;
-		this.from = from;
-		this.subject = subject;
-		this.action = action;
-	}
+    public emailConfirmation(String to, String from, String subject, String action) {
+        this.to = to;
+        this.from = from;
+        this.subject = subject;
+        this.action = action;
+    }
 
-	public void sendEmail() {
-		Properties properties = System.getProperties();
-		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.user", "admonitusconfirmation@gmail.com");
-		properties.put("mail.smtp.password", "anotherchancetonight");
-		properties.put("mail.smtp.port", "587");
-		properties.put("mail.smtp.auth", "true");
+    public void sendEmail() {
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.user", "admonitusconfirmation@gmail.com");
+        properties.put("mail.smtp.password", "anotherchancetonight");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
 
-		Session session = Session.getDefaultInstance(properties,
-				new javax.mail.Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(
-								"admonitusconfirmation@gmail.com",
-								"anotherchancetonight");
-					}
-				});
+        Session session = Session.getDefaultInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(
+                                "admonitusconfirmation@gmail.com",
+                                "anotherchancetonight");
+                    }
+                });
 
-		try {
-			MimeMessage message = new MimeMessage(session);
+        try {
+            MimeMessage message = new MimeMessage(session);
 
-			message.setFrom(new InternetAddress(from));
+            message.setFrom(new InternetAddress(from));
 
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(
-					to));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+                    to));
 
-			message.setSubject(subject);
+            message.setSubject(subject);
 
-			message.setContent("<body style='font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;'> <h2> Confirmation Message </h2> <br><hr> <center>Your reminder has been " + action + "</center><hr><p>Best Regards</p><p>Admonitus Team</p></body>", "text/html");
+            message.setContent("<body style='font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;'> <h2> Confirmation Message </h2> <br><hr> <center>Your reminder has been " + action + "</center><hr><p>Best Regards</p><p>Admonitus Team</p></body>", "text/html");
 
-			Transport.send(message);
-			System.out.println("Email sent");
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-	}
+            Transport.send(message);
+            System.out.println("Email sent");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public String getTo() {
-		return to;
-	}
+    public String getTo() {
+        return to;
+    }
 
-	public void setTo(String to) {
-		this.to = to;
-	}
+    public void setTo(String to) {
+        this.to = to;
+    }
 
-	public String getFrom() {
-		return from;
-	}
+    public String getFrom() {
+        return from;
+    }
 
-	public void setFrom(String from) {
-		this.from = from;
-	}
+    public void setFrom(String from) {
+        this.from = from;
+    }
 
-	public String getSubject() {
-		return subject;
-	}
+    public String getSubject() {
+        return subject;
+    }
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
